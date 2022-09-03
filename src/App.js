7da3d66d1c4cb899d5cf456083c3d97ada8296a0
Gustavo <romerogustavob@gmail.com>
2022-09-03 11:03:16 -0300

    'contador con boton'

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import { useEffect, useState } from 'react';
-import { Diagram2 } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
-  const [counter, setCounter]=useState(0);
-  const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-  const datetime2=current.toString();
-
-  useEffect(() => {      
-    setTimeout( ()=>{
-      console.log('Efecto al montaje')
-    },5000)
-  }, []);
-
-  useEffect(() => {  
-    console.log('Efecto al montaje y cambio el counter')
-    return () => {
-      
-    }
-  }, [counter]);
-  
-  useEffect(() => {  
-    console.log('Efecto en cada render')
-    return () => {
-      console.log('Efecto al desmontar')
-    }
-  });
-
-  const handleClick=()=>{
-    console.log('You are doing a click');
-    setCounter(counter + 1);
+  const addOnb = ({valor}) => {
+    alert('el valor actual es '+parseInt({valor}))
   }
-  console.log('hubo render');
   return (
     <>
     <div className='container'>
     <NavBar/>
-    <div>
-      <strong>Contador: {counter} -- {datetime2}</strong>
-    </div>
-    <Button variant="secondary" size="sm" active onClick={handleClick}>Click</Button>
-      <h2>Las ofertas de la semana</h2>
-      <ItemListContainer greeting={'Saludos'}/>
+    <ItemCount stock='5' inicio='1' addOn={addOnb}/>
     </div>
 
       
