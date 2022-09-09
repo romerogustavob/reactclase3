229f327a8c2712309e773d31cf1a88192ae9e1bf
Gustavo <romerogustavob@gmail.com>
2022-09-09 19:13:26 -0300

    clase router dom

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import SwapiContainer from './components/swapi/SwapiContainer';
+import NavBar from './components/NavBar';
+import { BrowserRouter, Switch, Route, Link, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   {/* <App />*/}
+  <BrowserRouter>
+  {/* <NavBar></NavBar> */}
+  <Routes>
+      <Route path='/' element={<App/>} />
+      <Route path='/swapi' element={<SwapiContainer/>} />
+      <Route path='/' element={<App/>} />
+      
+  </Routes>
+  <SwapiContainer/>
+</BrowserRouter>
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
